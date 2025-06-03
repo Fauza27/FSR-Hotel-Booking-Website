5f0c453e63d90b0bbdd1b2404bca5abcdfb19520
Fauza <140793971+Fauza27@users.noreply.github.com>
2025-06-03 17:28:16 +0800

    Delete views directory

--- views/admin/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-// views/admin/layouts/footer.php
-?>
-<footer style="text-align:center; margin-top:2rem; color:#888; font-size:0.9em;">
-    &copy; <?= date('Y') ?> Hotel Booking Admin
-</footer>
